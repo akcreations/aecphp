ab1e7709c32e84b1a1edfb8a444c9b4d87606523
Anantha Krishnan <geekanantha@gmail.com>
2018-03-18 11:08:55 +0530

    Add files via upload

---
+++ connections/connect.php
@@ -0,0 +1,12 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hack4i";
+// Create connection
+$con = new mysqli($servername, $username, $password,$dbname);
+
+
+
+?>
